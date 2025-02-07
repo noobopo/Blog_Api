54df49e3c596edfc8da391d67bf8f83ee7a37c69
shinchan-eng <kishalaydas784@gmail.com>
2025-02-07 12:56:06 +0530

    new update

--- utils/featuer.js
+++ utils/featuer.js
@@ -1,14 +1,18 @@
-import jwt from 'jsonwebtoken'
-export const generateCookie = (user,res,statusCode,message)=>{
-    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET)
-    res.status(statusCode).cookie("token",token,{
-        httpOnly:true,
-        maxAge:30*60*1000,
-        samSite:process.env.NODE_ENV === "Development" ? "lax":"none",
-        secure:process.env.NODE_ENV === "Development" ?false:true
+import jwt from 'jsonwebtoken';
 
-    }).json({
-        sucess:true,
-        message,
-    })
-}
+export const generateCookie = (user, res, statusCode, message) => {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '30m' });
+
+    res.status(statusCode)
+        .cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "Production",
+            sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+            maxAge: 30 * 60 * 1000, // 30 minutes
+            expires: new Date(Date.now() + 30 * 60 * 1000),
+        })
+        .json({
+            success: true,
+            message,
+        });
+};
